351d877f031d6815f25a91a638f4fd8b66e00e49
Nibrus Chowdhury <nibrusc@nycstudents.net>
2024-11-28 18:01:55 -0500

    Updated sophmore page and finished homepage

--- Sophmore Portfolio Page/sophmorePage.js
+++ Sophmore Portfolio Page/sophmorePage.js
@@ -13,29 +13,11 @@ button.addEventListener("click", function() {
 
 
 function firstClick() {
-    document.querySelector("#upArrowButton").style.backgroundColor = "#CDBE78";
-    document.querySelector("#semiCircle").style.borderTop = "none";
-    document.querySelector("#semiCircle").style.borderLeft = "none";
-    document.querySelector("#semiCircle").style.borderRight = "none";
-    document.querySelector("#semiCircle").style.borderBottom = "2px solid #CDBE78";
-    document.querySelector("#block").style.backgroundColor = "#CDBE78";
-    document.querySelector("#upArrow").style.borderColor = "#212121";
     document.querySelector("#upArrow").style.transform = "rotate(225deg)";
-    document.querySelector("#upArrow").style.zIndex = "1";
-    document.querySelector("#bottomSlide").style.bottom = "0px";
-    document.querySelector("#blocker").style.backgroundColor = "#CDBE78";
+    document.querySelector("#bottomSlide").style.bottom = "-50px";
 }
 
 function secondClick() {
-    document.querySelector("#bottomSlide").style.bottom = "-850px";
-    document.querySelector("#upArrowButton").style.backgroundColor = "transparent";
-    document.querySelector("#semiCircle").style.borderTop = "2px solid #CDBE78";
-    document.querySelector("#semiCircle").style.borderLeft = "2px solid #CDBE78";
-    document.querySelector("#semiCircle").style.borderRight = "2px solid #CDBE78";
-    document.querySelector("#semiCircle").style.borderBottom = "2px solid #212121";
-    document.querySelector("#block").style.backgroundColor = "#212121";
+    document.querySelector("#bottomSlide").style.bottom = "-870px";
     document.querySelector("#upArrow").style.transform = "rotate(45deg)";
-    document.querySelector("#upArrow").style.borderColor = "#CDBE78";
-    document.querySelector("#upArrow").style.zIndex = "0";
-    document.querySelector("#blocker").style.backgroundColor = "#212121";
 }
